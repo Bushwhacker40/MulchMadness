7cdeebf7e401e45f48d6230b23e4aaa48a5c9c41
Zach Carlson <carlson2038@gmail.com>
2026-05-22 20:51:03 -0500

    The beginning of Mulch Madness

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Mulch Madness</h1>
+     <h3>Coming to a yard near you...soon...</h3>
+    </>
+  )
+}
+
+export default App
